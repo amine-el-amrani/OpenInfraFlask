057e068dc88c1a55a838df7d2d89258bd9260531
Amine El amrani <amine_el_amrani@outlook.com>
2024-10-10 10:55:21 +0200

    Add OpenStack environment variables configuration and Flask application setup

--- cloud_api/cloud_api/__init__.py
+++ cloud_api/cloud_api/__init__.py
@@ -1,8 +1,28 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+import openstack
+
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
 
+    # Connexion à OpenStack avec les variables d'environnement
+    conn = openstack.connect(
+        auth_url=os.getenv('OS_AUTH_URL'),
+        project_name=os.getenv('OS_PROJECT_NAME'),
+        username=os.getenv('OS_USERNAME'),
+        password=os.getenv('OS_PASSWORD'),
+        user_domain_name=os.getenv('OS_USER_DOMAIN_NAME'),
+        project_domain_name=os.getenv('OS_PROJECT_DOMAIN_NAME'),
+        region_name=os.getenv('OS_REGION_NAME')
+    )
+
+    # Mettre la connexion à OpenStack dans la configuration de l'application
+    app.config['OPENSTACK_CONNECTION'] = conn
+
+    # Importer les routes
     from .routes import main_routes
     app.register_blueprint(main_routes)
 
